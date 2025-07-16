fc1cb9cc127ec886f3ce415853de30d24c79fb67
Satyam Jadon <satyamjadon001@gmail.com>
2025-07-16 14:27:37 +0530

    byedbhe

--- client/src/Components/RightSidebar.jsx
+++ client/src/Components/RightSidebar.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from '../../context/AuthContext'
 
 const RightSidebar = () => {
 
- const {selecteduser, messages}= useContext(ChatContext)
+ const {selectedUser, messages}= useContext(ChatContext)
  const {logout, onlineUsers} =useContext(AuthContext)
  const[ msgImages, setMsgImages] = useState([])
 
@@ -16,16 +16,16 @@ const RightSidebar = () => {
     )
   },[messages])
 
-  return selecteduser && (
+  return selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll 
-    {selecteduser ? "max-md:hidden" : ""}`}>
+    {selectedUser ? "max-md:hidden" : ""}`}>
 
     <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
-       <img src={selecteduser?.profilePic || assets.avatar_icon} alt="" 
+       <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" 
        className='w-20 aspect-[1/1] rounded-full'/>
        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-        {onlineUsers.includes(selecteduser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
-        {selecteduser.fullName}
+        {onlineUsers.includes(selectedUser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
+        {selectedUser.fullName}
         </h1>
         <p className='px-10 mx-auto'>{selecteduser.bio}</p>
     </div>
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-    sendersId: {type: mongoose.Schema.Types.ObjectId, ref: "User", require:true},
+    senderId: {type: mongoose.Schema.Types.ObjectId, ref: "User", require:true},
     receiverId: {type: mongoose.Schema.Types.ObjectId, ref: "User", require:true},
     text: {type: String,},
     image:{type: String,},
